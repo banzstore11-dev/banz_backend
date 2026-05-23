b82b9abb1f4ba06a4d89a7bd11fa2d7aa8b63ae8
ahsanmughal11 <sn092702@gmail.com>
2026-05-23 22:59:50 +0500

    connect to real domain

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,10 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => array_values(array_filter(array_map('trim', explode(',', (string) env(
+        'CORS_ALLOWED_ORIGINS',
+        'https://banzstore.com,https://www.banzstore.com'
+    ))))),
 
     'allowed_origins_patterns' => [],
 
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->away('https://banzstore.com', 301);
+});
+
+Route::fallback(function () {
+    return redirect()->away('https://banzstore.com', 301);
 });
